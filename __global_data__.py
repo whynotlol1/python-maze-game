2294581802bbcb9935f831aec6926e017690945b
cat dev <117660464+whynotlol1@users.noreply.github.com>
2023-09-07 23:24:58 +0300

    Update __global_data__.py

--- __global_data__.py
+++ __global_data__.py
@@ -3,17 +3,17 @@
 GAME = {
     "BOUNDS": 700,
     "UNIT_SIZE": 20,
-    "FPS_STANDART": 60,
-    "VERSION": "#0.0.3 beta",
+    "GAME_SPEED": 20,
+    "VERSION": "#0.0.4 beta",
     "IS_RUNNING": True
 }
 
 PLAYER = {
     "POSITION": {
-        "X": 10,
-        "Y": 10
-    },
-    "SPEED": 1
+        "X": GAME["UNIT_SIZE"],
+        "Y": GAME["UNIT_SIZE"]
+    },  # Starting position. Starting in the middle of the entrance area
+    "SPEED": GAME["UNIT_SIZE"]  # Temporary measure for the collision handler to work
 }
 
 COLORS = {
