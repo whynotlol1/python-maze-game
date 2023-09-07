11340d2a208e72c4c50faa40c1868e6a6fcd6756
cat dev <117660464+whynotlol1@users.noreply.github.com>
2023-09-07 19:07:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# feel free to use some parts of this code in your project
+# Feel free to use some parts of this code in your project
 
 import __global_data__
 import map_generation
@@ -8,15 +8,15 @@ import time
 
 
 def main() -> None:
-    # initialising pygame
+    # Initialising pygame
     pygame.init()
     screen = pygame.display.set_mode((__global_data__.GAME["BOUNDS"][0], __global_data__.GAME["BOUNDS"][1]))
     pygame.display.set_caption(f"Maze game v{__global_data__.GAME['VERSION']}")
     clock = pygame.time.Clock()
-    # generating the map
+    # Generating the map
     game_surface = map_generation.generate(width=__global_data__.GAME["BOUNDS"][1], height=__global_data__.GAME["BOUNDS"][0], iterations=1)
-    # generating entrance and exit points
-    # entrance
+    # Generating entrance and exit points
+    # Entrance
     game_surface[0][0] = 2
     game_surface[__global_data__.GAME["UNIT_SIZE"]][__global_data__.GAME["UNIT_SIZE"]] = 2
     game_surface[__global_data__.GAME["UNIT_SIZE"] * 2][__global_data__.GAME["UNIT_SIZE"] * 2] = 2
@@ -26,16 +26,16 @@ def main() -> None:
     game_surface[0][__global_data__.GAME["UNIT_SIZE"] * 2] = 2
     game_surface[__global_data__.GAME["UNIT_SIZE"]][0] = 2
     game_surface[__global_data__.GAME["UNIT_SIZE"] * 2][0] = 2
-    # exit
+    # Exit
     # TODO
 
     while __global_data__.GAME["IS_RUNNING"]:
         clock.tick(__global_data__.GAME["FPS_STANDART"])
-        # rendering
-        # filling the screen black
+        # Rendering
+        # Filling the screen black
         screen.fill(__global_data__.COLORS["BLACK"])
 
-        # rendering the maze part seen to the player
+        # Rendering the maze part seen to the player
         for x_pos in range(0, __global_data__.GAME["BOUNDS"][0], __global_data__.GAME["UNIT_SIZE"]):
             for y_pos in range(0, __global_data__.GAME["BOUNDS"][1], __global_data__.GAME["UNIT_SIZE"]):
                 DISTANCE = math.sqrt((x_pos - __global_data__.PLAYER["POSITION"]["X"]) ** 2 + (y_pos - __global_data__.PLAYER["POSITION"]["Y"]) ** 2)
@@ -49,15 +49,15 @@ def main() -> None:
                         case 2:  # entrance or exit
                             pygame.draw.rect(screen, __global_data__.COLORS["YELLOW"], (x_pos, y_pos, x_pos + __global_data__.GAME["UNIT_SIZE"], y_pos + __global_data__.GAME["UNIT_SIZE"]))
 
-        # rendering the player
+        # Rendering the player
         pygame.draw.circle(screen, __global_data__.COLORS["RED"], (__global_data__.PLAYER["POSITION"]["X"], __global_data__.PLAYER["POSITION"]["Y"]), int(__global_data__.GAME["UNIT_SIZE"] / 3))
 
         pygame.display.update()
 
-        # debug mode
+        # Debug mode [dev only]
         debug_mode = False
 
-        # controls
+        # Controls
         KEYS = pygame.key.get_pressed()
 
         if KEYS[pygame.K_w] and __global_data__.PLAYER["POSITION"]["Y"] > 0:  # [W]
@@ -72,7 +72,7 @@ def main() -> None:
         elif KEYS[pygame.K_d] and __global_data__.PLAYER["POSITION"]["X"] < __global_data__.GAME["BOUNDS"][1]:  # [D]
             __global_data__.PLAYER["POSITION"]["X"] += __global_data__.PLAYER["SPEED"]
 
-        elif KEYS[pygame.K_x]:  # enable / disable debug mode
+        elif KEYS[pygame.K_x]:  # Enable / disable debug mode
             debug_mode = True if not debug_mode else False
             if debug_mode:
                 __global_data__.PLAYER["VIEW"]["FACTOR"] = 100
@@ -82,11 +82,11 @@ def main() -> None:
                 __global_data__.PLAYER["VIEW"]["STRENGTH_FACTOR"] = 200
             time.sleep(0.1)
 
-        elif KEYS[pygame.K_ESCAPE]:  # quitting if [ESC] is pressed
+        elif KEYS[pygame.K_ESCAPE]:  # Quitting if [ESC] is pressed
             pygame.quit()
             __global_data__.GAME["IS_RUNNING"] = False
 
-        for event in pygame.event.get():  # quitting if X is pressed
+        for event in pygame.event.get():  # Quitting if X is pressed
             if event.type == pygame.QUIT:
                 pygame.quit()
                 __global_data__.GAME["IS_RUNNING"] = False
