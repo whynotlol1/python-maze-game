b0477eefe08835a225d076c1c35deb82b8532b4c
cat dev <117660464+whynotlol1@users.noreply.github.com>
2023-09-08 13:38:41 +0300

    Update __global_data__.py

--- __global_data__.py
+++ __global_data__.py
@@ -1,24 +1,27 @@
 # Feel free to use some parts of this code in your project
 
+# This is basically a configuration file. 
+# You can change some values here
+
 GAME = {
-    "BOUNDS": 700,
-    "UNIT_SIZE": 20,
-    "GAME_SPEED": 20,
-    "VERSION": "#0.0.4 beta",
-    "IS_RUNNING": True
+    "BOUNDS": 700,  # Keep an integer number
+    "UNIT_SIZE": 20,  # Keep an integer number less than BOUNDS
+    "GAME_SPEED": 20,  # Keep an integer number
+    "VERSION": "#0.0.4.1 beta",  # Do not change
+    "IS_RUNNING": True  # Do not change
 }
 
 PLAYER = {
     "POSITION": {
-        "X": GAME["UNIT_SIZE"],
-        "Y": GAME["UNIT_SIZE"]
+        "X": GAME["UNIT_SIZE"],  # Do not change
+        "Y": GAME["UNIT_SIZE"]  # Do not change
     },  # Starting position. Starting in the middle of the entrance area
-    "SPEED": GAME["UNIT_SIZE"]  # Temporary measure for the collision handler to work
+    "SPEED": GAME["UNIT_SIZE"]  # Temporary measure for the collision handler to work  # Do not change
 }
 
 COLORS = {
-    "BLACK": (0, 0, 0),
-    "WHITE": (255, 255, 255),
-    "YELLOW": (204, 204, 0),
-    "RED": (250, 50, 0)
+    "BLACK": (0, 0, 0),  # Only change if you know what you are doing
+    "WHITE": (255, 255, 255),  # Only change if you know what you are doing
+    "YELLOW": (204, 204, 0),  # Only change if you know what you are doing
+    "RED": (250, 50, 0)  # Only change if you know what you are doing
 }
