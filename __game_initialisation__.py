9f800169493ec380ea0d974d9037d89f6ba5fc0d
cat dev <117660464+whynotlol1@users.noreply.github.com>
2023-09-08 23:13:35 +0300

    Update __game_initialisation__.py

--- __game_initialisation__.py
+++ __game_initialisation__.py
@@ -6,5 +6,22 @@ pygame.init()
 pygame.font.init()
 my_font = pygame.font.SysFont('Roboto', 50)
 screen = pygame.display.set_mode((__global_data__.GAME["BOUNDS"], __global_data__.GAME["BOUNDS"]))
-pygame.display.set_caption(f"Maze game v{__global_data__.GAME['VERSION']}")
+name = f"Maze game v{__global_data__.GAME['VERSION']}"
+pygame.display.set_caption(name)
 clock = pygame.time.Clock()
+
+# Initialising buttons
+button_width = 100
+button_height = 50
+buttons = {
+    "QUIT": {
+        "TEXT": "Quit",
+        "ACTION_CODE": "QUIT",
+        "POSITION": [__global_data__.GAME["BOUNDS"] / 6, 300]
+    },
+    "PLAY": {
+        "TEXT": "Play",
+        "ACTION_CODE": "PLAY",
+        "POSITION": [(__global_data__.GAME["BOUNDS"] / 6) * 4, 300]
+    }
+}
